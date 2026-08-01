6d1c2d96d67fcf95ecb6840cc98d99d490595bcf
apoorvagupta1024-maker <apoorvagupta1024@gmail.com>
2026-08-01 16:09:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const envelope = document.getElementById("envelopeScreen");
 const home = document.getElementById("homeScreen");
 
+console.log(envelope);
+console.log(home);
+
 envelope.addEventListener("click", function () {
-
+    alert("Envelope clicked!");
     envelope.style.display = "none";
-
     home.style.display = "flex";
-
 });
