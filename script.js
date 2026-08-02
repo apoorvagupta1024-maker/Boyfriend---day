4426ebe3d5a818f4b22bc5180c7b59c6f4011821
apoorvagupta1024-maker <apoorvagupta1024@gmail.com>
2026-08-02 18:21:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,26 +2,36 @@
 const envelopeScreen = document.getElementById("envelopeScreen");
 const website = document.getElementById("website");
 const envelope = document.querySelector(".envelope");
+
 const beginBtn = document.getElementById("beginBtn");
 const chapter1 = document.getElementById("chapter1");
+
+const next1 = document.getElementById("next1");
+const chapter2 = document.getElementById("chapter2");
 
 // Open envelope
 envelope.addEventListener("click", () => {
 
-    // Play opening animation
     envelope.classList.add("open");
 
-    // After animation, show the website
     setTimeout(() => {
         envelopeScreen.style.display = "none";
         website.style.display = "flex";
+        window.scrollTo(0, 0);
     }, 900);
 
 });
 
-// Scroll to Chapter 1
+// Hero → Chapter 1
 beginBtn.addEventListener("click", () => {
     chapter1.scrollIntoView({
+        behavior: "smooth"
+    });
+});
+
+// Chapter 1 → Chapter 2
+next1.addEventListener("click", () => {
+    chapter2.scrollIntoView({
         behavior: "smooth"
     });
 });
